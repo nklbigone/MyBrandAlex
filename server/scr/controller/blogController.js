092279d8d22365333f17a6e0a8855c04963d597e
nkl_alexis <nklbigone@gmail.com.com>
2020-09-18 16:04:19 +0200

    query bug fixing

--- server/scr/controller/blogController.js
+++ server/scr/controller/blogController.js
@@ -13,6 +13,39 @@ class blogsController{
         }
     }
 
+    static async blogGetOneName(req , res){
+
+        try{
+
+            const blogs = await blog.findById(req.params.id)
+            res.json(blogs)
+    
+        }catch(err){
+        res.send('error' +err)
+        }
+    }
+
+
+    static async updateBlog(req, res) {
+        try {
+
+            const blogs = await blog.findById(req.params.id)
+            if (!blogs) return (res, 404, "doesn't find one blog")
+
+                blogs.title = req.body.title
+         
+                blogs.blogDescription = req.body.blogDescription
+            
+                blogs.blogPicture = req.body.blogPicture
+                const blogup = await blogs.save()
+                res.json(blogup)
+
+        } catch (e) {
+            return(res, 404, " fails")
+        }
+    }
+
+
     static async blogPostName(req, res){
         const {error} = blogValidation(req.body);
         if (error){
@@ -35,6 +68,20 @@ class blogsController{
             res.send('Error')
         }
     }
+
+    static async deleteBlog(req, res) {
+        try {
+
+            const blogs = await blog.findById(req.params.id)
+            if (!blogs) return (res, 404, "doesn't find one blog")
+                const blogup = await blogs.deleteOne()
+                res.json(blogup)
+
+        } catch (e) {
+            return(res, 404, " fails")
+        }
+    }
+    
 }
 
 export default blogsController
